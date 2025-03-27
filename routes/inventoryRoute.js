303fd84a315dcf9f42eeaf5f7aca7cf52d38cf47
Attah Collins <attahcollinso@gmail.com>
2025-03-27 12:58:51 +0100

    still working on week 03

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,8 +2,21 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
+
+
+// Route to build inventory view
+router.get("/",utilities.handleErrors(invController.buildManagement));
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId));
+
+
+router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification));
+router.get("/add-vehicle", utilities.handleErrors(invController.buildAddVehicle));
+router.get("/edit-vehicle/:inv_id", utilities.handleErrors(invController.buildEditVehicle));
+
 
 module.exports = router;
--- utilities/index.js
+++ utilities/index.js
@@ -1,5 +1,7 @@
 const invModel = require("../models/inventory-model")
 const Util = {}
+require("dotenv").config()
+
 
 /* ************************
  * Constructs the nav HTML unordered list
@@ -32,7 +34,8 @@ Util.buildClassificationGrid = async function(data){
   if(data.length > 0){
     grid = '<ul id="inv-display">'
     data.forEach(vehicle => { 
-      grid += '<li>'
+      // grid += '<li>'
+      grid += '<li id="classification-card">'
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
@@ -57,6 +60,67 @@ Util.buildClassificationGrid = async function(data){
   return grid
 }
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
+
+Util.buildInvDetails = async function (data){
+  let details = '<div id="inv-detail-main">';
+  details +=
+      '<img src="' +
+      data.inv_image +
+      '" alt="Image of ' +
+      data.inv_make +
+      " " +
+      data.inv_model +
+      ' on CSE Motors" />';
+  details += '<div class="inv-detail-sub">';
+  details +=
+      "<h2><strong>" +
+      data.inv_make +
+      " " +
+      data.inv_model +
+      " Details" +
+      "</strong></h2>";
+  details += "<ul>";
+  details +=
+      "<li><strong>Price: $" +
+      new Intl.NumberFormat("en-US").format(data.inv_price) +
+      "</strong></li>";
+      details +=
+      "<li><strong>Description: </strong>" + data.inv_description + "</li>";
+  details += "<li><strong>Color:</strong> " + data.inv_color + "</li>";
+  details +=
+      "<li><strong>Miles:</strong> " +
+      new Intl.NumberFormat("en-US").format(data.inv_miles) +
+      "</li>";
+  details += "</ul>";
+  details += "</div>";
+  details += "</div>";
+  return details;
+}
+
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications()
+  let classificationList =
+      '<select name="classification_id" id="classificationList" required>'
+  classificationList += "<option value=''>Choose a Classification</option>"
+  data.rows.forEach((row) => {
+      classificationList += '<option value="' + row.classification_id + '"'
+      if (
+          classification_id != null &&
+          row.classification_id == classification_id
+      ) {
+          classificationList += " selected "
+      }
+      classificationList += ">" + row.classification_name + "</option>"
+  })
+  classificationList += "</select>"
+  return classificationList
+}
 
 module.exports = Util
